aaa9f1614c75ef7b617df9d0b93364ce2860d715
mdyasinbhuiyan112233@gmail.com <mdyasinbhuiyan112233@gmail.com>
2022-12-30 08:20:06 +0600

    whileLoop

---
+++ js/forLoop2.js
@@ -0,0 +1,7 @@
+var m = parseInt(prompt());
+var n = parseInt(prompt());
+var sum = 0;
+for (var i = m; i <= n; i++){
+  sum = sum + i;
+}
+document.write(sum);
---
+++ js/whileLoop.js
@@ -0,0 +1,11 @@
+var sum = 0;
+var i = 1;
+while (i <= 100){
+  if(i % 3 == 0 && i % 5 == 0){
+    sum = sum + i;
+  }else{
+    document.write()
+  }
+  i++;
+}
+document.write(sum);;
