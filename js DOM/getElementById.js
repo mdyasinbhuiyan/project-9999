1e983689748503cab5889d529a405eeb5c11e5b7
mdyasinbhuiyan112233@gmail.com <mdyasinbhuiyan112233@gmail.com>
2023-01-25 21:41:26 +0600

    g

--- js DOM/getElementById.js
+++
@@ -1,8 +0,0 @@
-let heading1 = document.getElementById("heading1");
-heading1.innerHTML = "There is nothing..."
-
-let heading2 = document.getElementById("heading2");
-heading2.innerHTML = "Good Bye! " + "Good Bye!";
-
-let para1 = document.getElementById("para1");
-para1.innerHTML = "bye! bye!"
--- js DOM/getElementsByTàgName.js
+++
@@ -1,3 +0,0 @@
-document.getElementsByTagName("h1")[0].innerHTML = "HI";
-
-document.getElementsByClassName("head")[1].innerHTML = "HIM";
